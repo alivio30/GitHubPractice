e3e3b3e2b03c322e8b13facb7a5bcbdb3bbc43bc
Igoy <christianigoy.official2701@gmail.com>
2023-01-30 22:11:56 +0800

    Full Name inputted

--- app/src/main/java/com/example/githubpractice/MainActivity.java
+++ app/src/main/java/com/example/githubpractice/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         //comment ni alivio
         //
         //e comment inyong full name ubos ani, igoy
-        //
+        // Christian Alynn Igoy
         //e comment inyong full name ubos ani, yumang
         //
     }
